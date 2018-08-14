9674b2ace4c2d72db596e8a0f086b987c0d93d87
James Mount <j.mount@qut.edu.au>
2018-08-14 19:15:33 +1000

    Updated change log and added remoteAPI folder to github to try and work on a solution for Hardware in the Loop.

---
+++ VREP_Sim/remoteApi/build/CMakeFiles/RemoteApi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RemoteApi.dir/extApiPlatform.c.o"
+  "CMakeFiles/RemoteApi.dir/extApi.c.o"
+  "lib/libRemoteApi.pdb"
+  "lib/libRemoteApi.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RemoteApi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
